4b44a9f1f55473f601cfe4d29c68dce6196ead47
namanhishere <53372786+namanhishere@users.noreply.github.com>
2024-01-17 20:19:58 +0700

    upload homework week1

---
+++ week1/persudoCode.py
@@ -0,0 +1,18 @@
+#Name: Trần Nam Anh
+#Student ID: 2352068
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+
+if(a > b):
+    if(a>c):
+        print(a)
+    else:
+        print(c)
+else:
+    if(b>c):
+        print(c)
+    else:
+        print(b)
